0d31dc210225e75f562c11e0908709e1fc69dd98
Ricardo Silva <ricardosilva@Ricardos-MacBook-Air.local>
2020-03-30 21:13:03 +0100

    api

--- api/src/controllers/incidentsControllers.js
+++ api/src/controllers/incidentsControllers.js
@@ -1,7 +1,7 @@
 const connection = require('../database/connection')
 
 module.exports = {
-    async index (req, res){
+    async index(req, res) {
         const { page = 1 } = req.query
 
         const [count] = await connection('incidents').count('id')
@@ -13,30 +13,30 @@ module.exports = {
             .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.phone', 'ongs.city', 'ongs.district'])
 
         res.header('X-Total-Count', count['count(`id`)'])
-        
+
         return res.json(incidents)
     },
 
-    async create (req, res){
+    async create(req, res) {
         const ong_id = req.headers.authorization
         const { title, description, value } = req.body
-        
-        const [id] = await connection('incidents').insert({title, description, value, ong_id})
-        res.json({id})
+
+        const [id] = await connection('incidents').insert({ title, description, value, ong_id })
+        res.json({ id })
     },
 
-    async delete(req, res){
+    async delete(req, res) {
         const ong_id = req.headers.authorization
 
-        const {id} = req.params
-        
+        const { id } = req.params
+
         const incident = await connection('incidents').where('id', id).select('ong_id').first()
 
-        if(!incident){
-            return res.status(404).json({error:"Incident not found"})
+        if (!incident) {
+            return res.status(404).json({ error: "Incident not found" })
         }
-        if(incident.ong_id !== ong_id){
-            return res.status(401).json({error:'Operation not permited'})
+        if (incident.ong_id !== ong_id) {
+            return res.status(401).json({ error: 'Operation not permited' })
         }
 
         await connection('incidents').where('id', id).delete()
