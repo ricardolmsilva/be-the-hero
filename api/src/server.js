e6f5482bfa423fe96a03a3c6cb32822c827eb9e8
Ricardo Silva <ricardosilva@Ricardos-MacBook-Air.local>
2020-04-15 13:55:15 +0100

    add env variable and example incidents in bd

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,6 @@
+require('dotenv/config');
 const app = require('./app');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${5000}!`));
+app.listen(port, () => console.log(`Server running on port ${port}!`));
