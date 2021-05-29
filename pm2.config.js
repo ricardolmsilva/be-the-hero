6a527fb2cbc46bddabb08a1b71feeb67c1f69895
Ricardo <ricardo.silva@codecomputerlove.com>
2021-05-29 13:21:05 +0100

    remove mobile from pm2

--- pm2.config.js
+++ pm2.config.js
@@ -1,30 +1,30 @@
 module.exports = {
   apps: [
     {
-      name: 'hero-server',
-      script: './api/src/server.js',
+      name: "hero-server",
+      script: "./api/src/server.js",
       env: {
         PORT: 8004,
         SALT_ROUNDS: 4,
-        JWT_SECRET: 'secret',
-        NODE_ENV: 'production',
-      }
-    },
-    {
-      name: 'hero-client',
-      script: 'serve',
-      env: {
-        PM2_SERVE_PATH: './web/build',
-        PM2_SERVE_PORT: 8003,
-        PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: '/index.html',
+        JWT_SECRET: "secret",
+        NODE_ENV: "production",
       },
     },
     {
-      name: 'hero-mobile',
-      cwd: "./mobile",
-      script: "expo",
-      args: "start --no-dev --minify",
+      name: "hero-client",
+      script: "serve",
+      env: {
+        PM2_SERVE_PATH: "./web/build",
+        PM2_SERVE_PORT: 8003,
+        PM2_SERVE_SPA: "true",
+        PM2_SERVE_HOMEPAGE: "/index.html",
+      },
     },
+    // {
+    //   name: 'hero-mobile',
+    //   cwd: "./mobile",
+    //   script: "expo",
+    //   args: "start --no-dev --minify",
+    // },
   ],
 };
